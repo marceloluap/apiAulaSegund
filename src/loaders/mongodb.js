f18f825fd1817963e12f063fdb8779fc25494d33
marceloluap <moluap@trustprev.com.br>
2023-04-03 00:16:45 -0300

    banco 0.0.0.0

--- src/loaders/mongodb.js
+++ src/loaders/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function startDB() {
-    await mongoose.connect('mongodb+srv://marceloluap:marceloluap@cluster0.kz0vybw.mongodb.net/test');
+    await mongoose.connect('mongodb+srv://marceloluap:123321@cluster0.kjkswvj.mongodb.net/test');
 };
 
 module.exports = startDB;
